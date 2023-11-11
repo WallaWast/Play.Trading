43de9b0348d1b5b5e0ed7802539b23f44f253e54
Wallace Torres <wallacewst@gmail.com>
2023-11-11 10:48:01 -0800

    Creation

---
+++ src/Play.Trading.Service/Contracts.cs
@@ -0,0 +1,7 @@
+using System;
+
+namespace Play.Trading.Service.Contracts;
+
+public record PurchaseRequested(Guid UserId, Guid ItemId, int Quantity, Guid CorrelationId);
+
+public record GetPurchaseState(Guid CorrelationId);
---
+++ src/Play.Trading.Service/Exceptions/UnknownItemException.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Play.Trading.Service.Exceptions;
+
+[Serializable]
+internal class UnknownItemException : Exception
+{
+    public UnknownItemException(Guid itemId) : base($"Unknown item '{itemId}'")
+    {
+        ItemId = itemId;
+    }
+
+    public Guid ItemId { get; }
+}
