43f7996693b74450e17074bfeef368b4ea21cff8
Wallace Torres <wallacewst@gmail.com>
2023-11-26 15:36:44 -0800

    Send logs to Seq

---
+++ src/Play.Trading.Service/Settings/SeqSettings.cs
@@ -0,0 +1,14 @@
+namespace Play.Trading.Service.Settings;
+
+public class SeqSettings
+{
+    public string Host { get; init; }
+    public int Port { get; init; }
+    public string ServerUrl
+    {
+        get
+        {
+            return $"http://{Host}:{Port}";
+        }
+    }
+}
